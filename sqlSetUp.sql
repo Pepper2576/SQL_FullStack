188a306c88e171bacd5bdc891cd7a07981ffc52a
Chris Pepper <pepper2576@gmail.com>
2025-01-21 16:30:45 +0000

    sql setup instructions

---
+++ sqlSetUp.sql
@@ -0,0 +1,14 @@
+create database mydb;
+
+use mydb;
+
+create table employees (
+	id int,
+    first_name varchar(50),
+    last_name varchar(50),
+    admin_rights tinyint
+	);
+    
+    insert into employees value (1, "Hello", "World", 1);
+    
+    select * from employees;
